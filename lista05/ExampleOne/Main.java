1ccad9fef58fbe408666ff5df14013594ae84478
Agnieszka Piotrowska <agnieszka.piotrowska@studenci.collegiumwitelona.com>
2025-04-06 19:48:36 +0200

    switch jako instrukcja bez przekazywania sterowania

--- lista05/ExampleOne/Main.java
+++ lista05/ExampleOne/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        MyData.info();
+
+        System.out.println("Switch jako instrukcja\n(bez przekazywania sterowania)");
+        int numLetters;
+
+      //  String seasonName = "Wiosna".toLowerCase();
+        String seasonName = "Przedwiośnie".toLowerCase();
+        switch (seasonName) {
+            case "wiosna" ->{
+                System.out.println("Wiosa wiosna wiosna ach to ty");
+                numLetters = 6;
+            }
+            case "lato","zima"-> numLetters=4;
+            case "jesień"-> numLetters=6;
+            default -> numLetters = -1;
+        }
+        System.out.println(numLetters);
+    }
 }
